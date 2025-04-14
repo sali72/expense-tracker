2797baa1cd6f7147176056b4e59b038c7308d48e
Ali <sahashemi072@gmail.com>
2025-04-14 12:16:37 +0330

    feat: enhance FastAPI initialization with title and description for Expense Tracker API

--- app/main.py
+++ app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 
 from app.api.main import api_router
 
-app = FastAPI()
+app = FastAPI(
+    title="Expense Tracker",
+    description="Expense Tracker API"
+)
 
 app.include_router(api_router)
