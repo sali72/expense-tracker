d3c9c25079aff29bf9e55ff700ae7b3c2e7b1625
Ali <sahashemi072@gmail.com>
2025-04-16 07:37:03 +0330

    chore: add EXPENSE_TRACKER_URL for local testing in config.py

--- app/core/config.py
+++ app/core/config.py
@@ -13,6 +13,8 @@ class Settings(BaseSettings):
     AUTH_SERVICE_URL: str = "http://auth-service:8000"
     AUTH_SERVICE_TOKEN_URL: str = f"{AUTH_SERVICE_URL}/api/v1/login/access-token"
     
+    # for testing
+    EXPENSE_TRACKER_URL: str = "http://127.0.0.1:8000"
     TEST_DB_NAME: str = MONGODB_DB_NAME + "_test"
     MOCK_TOKEN: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDM4NjQwMzksInN1YiI6ImFhOTA5MjFmLWQ2YjktNDhkOC05M2M2LWZjYTg5M2ZjMGNiMCJ9.Hl6cd7KsH-gGrjVZsjE0txs26qm6jsMzMrUfh0lQwpI"
 
